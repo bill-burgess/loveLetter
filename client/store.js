68df9429b5fd2d3441ce12656f9de573ebee32ba
anna-marie-vos <amvos@outlook.com>
2017-01-11 16:40:52 +1300

    refactored and fixed a few bugs

--- client/store.js
+++ client/store.js
@@ -8,16 +8,16 @@ module.exports = function (model) {
     subscribe: function (listener) {
       listeners.push(listener)
     },
-
-    update: function (key, update) {
-      console.log('model before update:', model)
-      model[key] = update
-
-      console.log('model after:', model)
-
-      this.notify(model)
-    },
-
+  //
+  //   update: function (key, update) {
+  //     console.log('model before update:', model)
+  //     model[key] = update
+  //
+  //     console.log('model after:', model)
+  //
+  //     this.notify(model)
+  //   },
+  //
     notify: function (model) {
       console.log('listener[0]', listeners[0])
 
